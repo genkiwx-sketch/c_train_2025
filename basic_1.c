78490c6d34f0e618c82fbfaf3ea7156c518d0e04
genkiwx-sketch <genki.wx@gmail.com>
2025-09-20 23:01:34 +0900

    Add files via upload

---
+++ basic_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+typedef short int sh;
+int main (void)
+{   int a,b;
+    sh c;
+
+   a=12;
+   b=5;
+   c=a*b;
+ 
+  printf("%d \n",c);
+
+ 
+ return 0;
+
+}
+
---
+++ basic_2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+enum number{
+    zero,
+    one, 
+    two
+};
+int main (void)
+{   
+   enum number n ;
+   n=two;
+  printf("%d \n",n);
+
+ 
+ return 0;
+
+}
+
